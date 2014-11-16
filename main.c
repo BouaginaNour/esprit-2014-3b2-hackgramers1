bed28cf42547566893ceb3b64046fb0700b506c3
nour.bouagina@esprit.tn <nour.bouagina@esprit.tn@ed06ec1c-f991-4c58-fa5d-4ac669f0e535>
2014-11-16 18:02:17 +0000

    travail final !

--- main.c
+++ main.c
@@ -1,23 +1,36 @@
 #include <stdio.h>
-#include "gestion_garage.h" 
-#include "voiture.h" 
-#include "menu.h"
-#define MAX 100 
-int main() 
-{
-      voiture garage[MAX];
-      int n = 0;
+ #include "gestion_garage.h"
+ #include "voiture.h"
+ #include "menu.h"
+#define MAX 100
+int main()
+ {
+voiture garage[MAX];
+ int n = 0;
 int choix;
-switch (choix) {
-case 1: ajouter_voiture( garage[], n);
-break;
-case 2: supprimer_voiture( garage[],  immatriculation[], n) ;
-break;
-case 3: afficher_garage(garage[],n) ;
-break;
-case 4: init_garage(garage[],n);
-break;
-case 0: printf("\nVous quitter l'application"); break;
-default: printf("\nmauvais choix");
+voiture v ;
+char immatriculation[30] ;
+do
+{
+ afficher_menu() ;
+choix= choix_menu();
+switch (choix)
+ {
+
+ case 1: ajouter_voiture ( garage, &n, v);
+ break;
+ case 2: supprimer_voiture( garage,immatriculation, &n) ;
+ break;
+ case 3: afficher_garage( garage, n) ;
+ break;
+ case 4: init_garage( garage, n);
+ break;
+ case 0: printf("\nVous quitter l'application");
+ break;
+ default: printf("\nmauvais choix");
 }
-return (0); }
+}
+while (choix!=0) ;
+return (0);
+}
+
